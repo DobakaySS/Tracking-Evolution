7181c73133a2b3b94dbcbf43dcc054dc557b6e95
Douglas de Sá <doug.xz@hotmail.com>
2024-01-29 18:40:16 -0300

    feat: add input binding for cards

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+ 
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  @Input('activity') activityName="";
+  @Input('color') activityColor="";
+  @Input('thisWeekHours') thisWeekHours=0;
+  @Input('lastWeekHours') lastWeekHours=0;
+  @Input() svgContent: string = '';
 
 }
