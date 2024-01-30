7468e314c63f722405f19438a7a138603517a9d5
Douglas de Sá <doug.xz@hotmail.com>
2024-01-30 11:18:08 -0300

    feat: profile-card component styles and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    ProfileCardComponent
   ],
   imports: [
     BrowserModule,
