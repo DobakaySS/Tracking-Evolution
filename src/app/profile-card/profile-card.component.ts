2f1b7bad02b9a80f2634e8be11ed1ac5750177a4
Douglas de Sá <doug.xz@hotmail.com>
2024-01-30 11:25:55 -0300

    feat: add input binding for profile-cards

--- src/app/profile-card/profile-card.component.ts
+++ src/app/profile-card/profile-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile-card',
@@ -11,5 +11,8 @@ export class ProfileCardComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @Input('name') profileName ="";
+  @Input('imagePath') imagePath ="";
 
 }
