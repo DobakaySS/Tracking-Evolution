eac85c3f9fd355414634b21fbe8126e9ccdf95e7
Douglas de Sá <doug.xz@hotmail.com>
2024-09-05 12:15:09 -0300

    feat: create router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggedHomePageComponent } from './logged-home-page/logged-home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: LoggedHomePageComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
